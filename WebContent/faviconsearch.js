e3ec25ba68be589acf31ede71d9f1eeda2bada9e
abhinit <abhinitk@buffalo.edu>
2016-01-14 23:19:36 -0500

    change url in js file

--- WebContent/faviconsearch.js
+++ WebContent/faviconsearch.js
@@ -4,8 +4,10 @@
 
 $(document).ready(function(){
 	
-	var url = "http://localhost:8080/FaviconSearchEngine/favicon/v1/rest/getUrl";
-	var webLinksUrl = "http://localhost:8080/FaviconSearchEngine/favicon/v1/rest/webUrl";
+	//var url = "http://localhost:8080/FaviconSearchEngine/favicon/v1/rest/getUrl";
+	var url = "https://cryptic-shelf-6360.herokuapp.com/favicon/v1/rest/getUrl/";
+	//var webLinksUrl = "http://localhost:8080/FaviconSearchEngine/favicon/v1/rest/webUrl";
+	var webLinksUrl = "https://cryptic-shelf-6360.herokuapp.com/favicon/v1/rest/webUrl";
 	var submitbutton = $("#submit");
 	var textbox = $("#link");
 	var link = "";
